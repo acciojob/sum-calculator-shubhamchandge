48dcf856c4e4d27922e86f6276643ebc58e6ea1c
shubhamchandge <shubhamchandge@gmail.comm>
2023-03-16 11:23:19 +0530

    add solution

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 
 import React from "react";
 import './../styles/App.css';
+import Sum from "./Sum";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <Sum />
     </div>
   )
 }
---
+++ src/components/Sum.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+
+
+const Sum = () => {
+
+    const [number, setNumber] = useState(0);
+    const [addition, setAddition] = useState(0);
+
+    const updatenumber = (e) => {
+        const num = e.target.value;
+        setNumber(num);
+        updateSum(num);
+    }
+
+    const updateSum = (num) => {
+        setAddition(parseInt(addition) + parseInt(num))
+    }
+
+    return (
+        <div className='container'>
+            <h1>Sum Calculator</h1>
+
+
+            <div className='inputbox'>
+                <input type={"number"} value={number} onChange={updatenumber} ></input>
+            </div>
+            <h3>sum:{addition} </h3>
+        </div>
+    )
+}
+
+export default Sum
